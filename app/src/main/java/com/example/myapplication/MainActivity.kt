0f3f1115d617389294d7ef9a2a8560f640e7815c
Sota Naoi <sota.naoi@juge6.jp>
2023-08-13 11:48:42 +0900

    add Informatin Logs

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,18 +6,23 @@ import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
-
+    val appTag: String = "TestApp"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         findViewById<Button>(R.id.button)
             .setOnClickListener {
                 exportDebugLog("ButtonClicked!!!")
+                exportInformationLog("ButtonClicked!")
             }
         exportDebugLog("OnCreate")
     }
 
     private fun exportDebugLog(message :String){
-        Log.d("TestApp", message)
+        Log.d(appTag, message)
+    }
+
+    private fun exportInformationLog(message: String){
+        Log.i(appTag, message)
     }
 }
